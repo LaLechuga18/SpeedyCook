6c1369beae8733912922b82728bfc66f26446699
Alan Ponce <95234211+KALLIIOZ@users.noreply.github.com>
2024-05-20 18:15:16 -0600

    Add files via upload

--- SpeeedyCookAPI.py
+++ SpeeedyCookAPI.py
@@ -1,42 +1,37 @@
-import requests, os
+import requests
 import json
+from googletrans import Translator
 
-
-def Edamam_API_call(comida, ingredintes, tipo, tiempo):
-    url='https://api.edamam.com/api/recipes/v2'
-    params={
-        'type':'any',
-        'app_id':'f93b1901',
-        'app_key':'c5695c359ddf80efb745044e4f9b1184',
-        'q':f'{comida}',
-        'ingr':f'{ingredintes}',
-        'mealType':f'{tipo}',
-        'time':f'{tiempo}'
+def Edamam_API_call():
+    url = 'https://api.edamam.com/api/recipes/v2'
+    params = {
+        'type': 'public',
+        'app_id': 'f93b1901',
+        'app_key': 'c5695c359ddf80efb745044e4f9b1184',
+        'q': 'steak',
+        'ingr': '4',
+        'mealType': 'Breakfast',
+        'time': '30'
     }
-    headers={"Content-Language":"es"}
 
-    response_food=requests.get(url, params=params, headers=headers)
+    response_food = requests.get(url, params=params)
     data_dict = response_food.json()
+    
+    translator = Translator()
 
     # Imprimir solo lo que sigue después de cada clave de interés
     if 'hits' in data_dict:
         for hit in data_dict['hits']:
             recipe = hit['recipe']
-            os.system('cls')
-            print(f'Tus ingreientes fueron: {comida}')
-            print("\nTítulo de la receta:", recipe['label'])
+            title_translated = translator.translate(recipe['label'], dest='es').text
+            print("\nTítulo de la receta:", title_translated)
+            
             print("Ingredientes:")
             for ingredient in recipe['ingredients']:
-                print("- ", ingredient['text'])
-                
-def get_food():
-    pass
+                ingredient_translated = translator.translate(ingredient['text'], dest='es').text
+                print("- ", ingredient_translated)
 
 def main():
-    meal=input('Inserte que ingreientes quiere que haya (use espacio para separar los ingredintes): ')
-    ingr=int(input('Inserte la cantidad maxima de ingredientes que queire que haya: '))
-    type=input('Inserte el tipo de comida que quiere (Breakfast, Dinner, Lunch, Snack, Teatime): ')
-    time=int(input('Inserte el tiempo paximo que tiene para preparse su comida: '))
-    Edamam_API_call(meal, ingr, type, time)
+    Edamam_API_call()
 
 main()
